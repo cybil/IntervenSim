9988dc9d99676019cf4ea5d136c0294379b9069b
Jean-Baptiste Laurent <jb@p1sec.com>
2013-11-22 02:01:24 +0100

    Adding strategy abstraction and first strategy

---
+++ Model/StratOldestUrgency.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class StratOldestUrgency extends Strategy
+{
+  public StratOldestUrgency(Map map)
+  {
+    super(map);
+  }
+
+  ArrayList<int []>	getPath()
+  {
+    ArrayList<int []>	path = null;
+
+    return (path);
+  }
+}
---
+++ Model/Strategy.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+
+abstract class Strategy
+{
+  protected int	_currentStrategy;
+  protected Map	_map;
+  public Strategy(Map map)
+  {
+    this._map = map;
+  }
+
+  /*!
+  ** Get the current strategy used
+  */
+  public int		getCurrStrategy()
+  {
+    return (this._currentStrategy);
+  }
+
+  /*!
+  ** Set the current strategy to use
+  */
+  public int		setCurrStrategy(int strategy)
+  {
+    this._currentStrategy = strategy;
+    return (this._currentStrategy);
+  }
+
+  /*!
+  ** Return the path to a node define by the strategy
+  ** Ex:Return the path to go to the oldest urgency
+  */
+  abstract ArrayList<int []>	getPath();
+
+  /*!
+  ** Return the oldest urgency on the map
+  */
+  protected Urgency	getOldestUrgency()
+  {
+    Node		node = null;
+    Urgency		OldestUrgency = null;
+
+    node = this.getOldestNodeUrgency();
+    if (node != null)
+      OldestUrgency = node.getNextUrgency();
+    return (OldestUrgency);
+  }
+
+  /*!
+  ** Return the node with the oldest urgency
+  */
+  protected Node	getOldestNodeUrgency()
+  {
+    Node		OldestNode = null;
+
+    for (Node node: this._map.getNodeUrgency())
+    {
+      if (OldestNode == null
+	  || node.getNextUrgency().getTriggerDate() > OldestNode.getNextUrgency().getTriggerDate())
+	OldestNode = node;
+    }
+    return (OldestNode);
+  }
+}
