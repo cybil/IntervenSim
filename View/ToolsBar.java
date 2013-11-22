af8e2dbfb9b79bff6fd049bb272c70d1169d8231
Melanie Ducani <melanieducani@gmail.com>
2013-11-21 21:32:10 -0500

    ToolsBar

---
+++ View/ToolsBar.java
@@ -0,0 +1,11 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+
+public class ToolsBar extends JPanel {
+	
+	ToolsBar() {
+		this.setPreferredSize(new Dimension(80, 250));
+		this.setBackground(new Color(0, 255, 0));
+	}
+}
