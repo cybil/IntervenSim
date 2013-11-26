2d8640d9d1a57607a2631d5adad4367e5b553072
Melanie Ducani <melanieducani@gmail.com>
2013-11-26 14:31:56 -0500

    Ajout gestion bouttons tools

--- View/ToolsBar.java
+++ View/ToolsBar.java
@@ -1,11 +1,24 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.GridLayout;
 
 public class ToolsBar extends JPanel {
-	
+	private SelectButton selectButton = new SelectButton();
+	private VehiculeButton vehiculeButton = new VehiculeButton();
+	private RoadButton roadButton = new RoadButton();
+	private NodeButton nodeButton = new NodeButton();
+
 	ToolsBar() {
 		this.setPreferredSize(new Dimension(80, 250));
 		this.setBackground(new Color(0, 255, 0));
+
+		this.setLayout(new GridLayout(4, 1));
+	    this.add(selectButton);
+	    this.add(vehiculeButton);
+	    this.add(roadButton);
+	    this.add(nodeButton);
+	    this.setVisible(true);
 	}
 }
