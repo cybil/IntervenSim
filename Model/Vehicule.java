dfae87bd492b2a139a9f81bc1ba95cace3048a25
Jean-Baptiste Laurent <jb@p1sec.com>
2013-11-26 17:36:49 +0100

    fonction Vehicule.moveOn() amélioré et debugger * fonctionne avec les tick d'horloge * débug des coordonnées et du calcul du kilométrage
    TODO: Gerer l'urgence une fois sur place et changement le status
    
    Ps: Bruler java qui ne gère pas les consts, qui ne sait pas passer un int par reference, qui faire des references sur les coordonnées sans prévenir, qui fait perdre des heures de debug, qui est pas logique (et qui a pas de message d'erreur clair)

--- Model/Vehicule.java
+++ Model/Vehicule.java
@@ -6,7 +6,7 @@ public class Vehicule implements java.io.Serializable {
     WAITING, ON_THE_ROAD, WORKING;
   }
 
-  private boolean	debug = true;
+  private boolean	debug = false;
   private float		km;
   private int[]		coord;
   private int[]		incomingCoord = {0, 0}; // The point from where the vehicule is comming
@@ -14,6 +14,18 @@ public class Vehicule implements java.io.Serializable {
   private ArrayList<Node>	path = new ArrayList<Node>();
   private int			speed;
   private EVehiculeState	state = EVehiculeState.WAITING;
+
+  // For the moveOn() methode
+  private boolean	initMoveOn = false;
+  private int	e2;
+  private int	dx;
+  private int	dy;
+  private int	err;
+  private int	dir_x;
+  private int	dir_y;
+  private int	x_dst;
+  private int	y_dst;
+  private Node	next_node;
 
   //******************
   //	Constructor
@@ -28,7 +40,7 @@ public class Vehicule implements java.io.Serializable {
   public Vehicule(float km, int[] coord,
 		  Node attachPoint, ArrayList<Node> path, int speed) {
     this.km = km;
-    this.coord = coord;
+    this.coord = coord.clone();
     this.attachPoint = attachPoint;
     this.incomingCoord = this.coord;
     this.path = path;
@@ -37,8 +49,8 @@ public class Vehicule implements java.io.Serializable {
 
   public Vehicule(Vehicule vehicule) {
     this.km = vehicule.km;
-    this.coord = vehicule.coord;
-    this.incomingCoord = this.coord;
+    this.coord = vehicule.coord.clone();
+    this.incomingCoord = this.coord.clone();
     this.attachPoint = vehicule.attachPoint;
     this.path = vehicule.path;
     this.speed = vehicule.speed;
@@ -76,13 +88,18 @@ public class Vehicule implements java.io.Serializable {
   }
 
   void		setPath(ArrayList<Node> newPath) {
+    int		last;
+
     if (newPath != null && newPath.size() > 0)
     {
+      last = this.path.size() - 1;
+      last = (last > 0 ? last : 0);
       this.state = EVehiculeState.ON_THE_ROAD;
       if (this.path.isEmpty())
 	this.path = newPath;
       else
 	this.path.addAll(newPath);
+      this.path.remove(last);
     }
     else
       System.out.println("Vehicule.setPath(): Error: Trying to set a null or empty path");
@@ -93,8 +110,10 @@ public class Vehicule implements java.io.Serializable {
   }
 
   void		setCoord(int[] newCoord) {
-    this.coord = newCoord;
-    this.incomingCoord = this.coord;
+    this.coord = newCoord.clone();
+    this.incomingCoord = this.coord.clone();
+    this.path.clear(); // The path is no longer valid
+    this.initMoveOn = false;
   }
 
   //***************
@@ -123,41 +142,48 @@ public class Vehicule implements java.io.Serializable {
       i = i + 1;
     }
     this.km += Math.sqrt((pythagore[0]) + (pythagore[1]));
+    if (debug == true) System.out.println("Vehicule.moveOn(): incoming src: "
+					  + this.incomingCoord[0] + ":"
+					  + this.incomingCoord[1]);
     this.incomingCoord = path.get(0).getCoord();
     if (debug == true) System.out.println("Vehicule.moveOn(): Updating km to " + this.km);
+  }
+
+  /*
+  ** Remove the reached point and update the status
+  */
+  private void		_update()
+  {
+    this._updatingKm();
+    this.path.remove(0);
+    this.initMoveOn = false;
   }
 
   public void		moveOn() {
     if (this.path != null && this.path.size() > 0 && this.isFree() == false)
     {
-      int	e2;
-      int	dx;
-      int	dy;
-      int	err;
-      int	dir_x;
-      int	dir_y;
-      int	x_dst;
-      int	y_dst;
-      Node	next_node;
-
-      if (debug == true) System.out.print("Vehicule.moveOn() path: ");
-      if (debug == true) AdjMatriceGen.prettyPrintPath(this.path);
-      x_dst = path.get(0).getCoord()[0];
-      y_dst = path.get(0).getCoord()[1];
-      dx = x_dst - coord[0];dx = (dx > 0 ? dx : -dx);
-      dy = y_dst - coord[1];dy = (dy > 0 ? dy : -dy);
-      dir_x = (x_dst > coord[0] ? 1 : -1);
-      dir_y = (y_dst > coord[1] ? 1 : -1);
-      err = dx - dy;
-
-      if (coord[0] == x_dst && coord[1] == y_dst) // If we are on the point, remove it from the path and go on
+// Init: If we are on the point, remove it from the path and go on
+      if (this.initMoveOn == false)
       {
-	this._updatingKm();
-	this.path.remove(0);
-	this.moveOn();
-	return;
+	if (debug == true) System.out.print("Vehicule.moveOn() from "
+					    + this.incomingCoord[0] + ":"
+					    + this.incomingCoord[1]
+					    + " path: ");
+	if (debug == true) AdjMatriceGen.prettyPrintPath(this.path);
+	x_dst = path.get(0).getCoord()[0];
+	y_dst = path.get(0).getCoord()[1];
+	dx = x_dst - coord[0];dx = (dx > 0 ? dx : -dx);
+	dy = y_dst - coord[1];dy = (dy > 0 ? dy : -dy);
+	dir_x = (x_dst > coord[0] ? 1 : -1);
+	dir_y = (y_dst > coord[1] ? 1 : -1);
+	err = dx - dy;
+	// this._update();
+	this.initMoveOn = true;
+	// if (this.path.size() > 0)
+	//   this.moveOn();
+	// return;
       }
-      while (!(coord[0] == x_dst && coord[1] == y_dst))
+      if (!(coord[0] == x_dst && coord[1] == y_dst))
       {
 	e2 = 2 * err;
 	if (e2 > -dy)
@@ -168,21 +194,17 @@ public class Vehicule implements java.io.Serializable {
 	    System.out.println("Vehicule.moveOn(): Moving on X to " + coord[0] + ":" + coord[1]
 			       + " (dst node = " + x_dst + ":" + y_dst + ")");
 	}
-	if (coord[0] == x_dst && coord[1] == y_dst)
+	if (!(coord[0] == x_dst && coord[1] == y_dst) && e2 < dx)
 	{
-	  this._updatingKm();
-	  this.path.remove(0);
-	  return;
-	}
-	if (e2 < dx)
-	{
-	  err = err + dx;
+	  err += dx;
 	  coord[1] += dir_y;
 	  if (this.debug == true)
 	    System.out.println("Vehicule.moveOn(): Moving on Y to " + coord[0] + ":" + coord[1]
 			       + " (dst node = " + x_dst + ":" + y_dst + ")");
 	}
       }
+      if (coord[0] == x_dst && coord[1] == y_dst)
+	this._update();
     }
     else if (path != null && path.size() == 0)
       System.out.println("Vehicule.moveOn(): Error: Trying to move whitout any path set."
