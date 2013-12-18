e6d861f2b7b1b807db3954ce1ba618ac8d25f254
Jean-Baptiste Laurent <jb@p1sec.com>
2013-12-18 13:07:38 +0100

    Bindage des 4 raccourcis du menu "file"

--- View/FileMenu.java
+++ View/FileMenu.java
@@ -15,136 +15,121 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class FileMenu extends JMenu {
-	private JMenuItem		newItem = new JMenuItem("New");
-	private JMenuItem		openItem = new JMenuItem("Open");
-	private JMenuItem		saveItem = new JMenuItem("Save");
-	private JMenuItem		saveAsItem = new JMenuItem("Save As...");
-	private JMenuItem		exportStatItem = new JMenuItem("Export Statistics");
-	private JMenuItem		exitItem = new JMenuItem("Exit");	
-	private JFileChooser 	fc = new JFileChooser();
-	private JFrame 			openDialog = new JFrame();
-	private File file;
-	
-	FileMenu(String str) {
-		setText(str);
-		this.setMnemonic(KeyEvent.VK_F);
-		
-		newItem.addActionListener(new NewItemAction());
-		newItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_DOWN_MASK));
-		this.add(newItem);
+  private JMenuItem		newItem = new JMenuItem("New");
+  private JMenuItem		openItem = new JMenuItem("Open");
+  private JMenuItem		saveItem = new JMenuItem("Save");
+  private JMenuItem		saveAsItem = new JMenuItem("Save As...");
+  private JMenuItem		exportStatItem = new JMenuItem("Export Statistics");
+  private JMenuItem		exitItem = new JMenuItem("Exit");
+  private JFileChooser 	fc = new JFileChooser();
+  private JFrame 			openDialog = new JFrame();
+  private File file;
+  private boolean		debug = false;
 
-		openItem.addActionListener(new OpenItemAction());
-		openItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_DOWN_MASK));
-		this.add(openItem);
+  FileMenu(String str) {
+    setText(str);
+    this.setMnemonic(KeyEvent.VK_F);
 
-		addSeparator();
+    newItem.addActionListener(new NewItemAction());
+    newItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_DOWN_MASK));
+    this.add(newItem);
 
-		saveItem.addActionListener(new SaveItemAction());
-		saveItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK));
-		this.add(saveItem);
+    openItem.addActionListener(new OpenItemAction());
+    openItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_DOWN_MASK));
+    this.add(openItem);
 
-		saveAsItem.addActionListener(new SaveAsItemAction());
-		saveAsItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK + InputEvent.SHIFT_DOWN_MASK));
-		add(saveAsItem);
+    addSeparator();
 
-		addSeparator();
+    saveItem.addActionListener(new SaveItemAction());
+    saveItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK));
+    this.add(saveItem);
 
-		exportStatItem.addActionListener(new ExportStatItemAction());
-		add(exportStatItem);
+    saveAsItem.addActionListener(new SaveAsItemAction());
+    saveAsItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK + InputEvent.SHIFT_DOWN_MASK));
+    add(saveAsItem);
 
-		addSeparator();
+    addSeparator();
 
-		exitItem.addActionListener(new ExitItemAction());
-		add(exitItem);
-	}
+    exportStatItem.addActionListener(new ExportStatItemAction());
+    add(exportStatItem);
 
-	public class NewItemAction implements ActionListener {
-		public void actionPerformed(ActionEvent evt) {
-			System.out.print("newItem\n");
-			// check si chgment  depuis dernier save ou si fichier non save
-			// si oui fenetre;
-			// si user ok
-			// save
-			// sinon quitte
-			
-			//	if ((b = comparetosavedFilemap()) {
-			//		openWinDiag()
-			//		if (ok) {
-			//		openSave();		
-			//		}
-			//	}
-			// exit
-			}
-		}
-	
-	// appeler existeFileMap => Map
-	// saveMap => FileManager
-	// loadMap = FileManager
-	public class OpenItemAction implements ActionListener {
-		public void actionPerformed(ActionEvent evt) {
-			System.out.print("openItem\n");
-			
-			//	int returnVal = fc.showOpenDialog(openDialog);
-			//	if (returnVal == JFileChooser.APPROVE_OPTION) {
-			//	open()
-			//	} else {
-			//	
-			// }
-			
-//					file = fc.getSelectedFile();//This is where a real application would open the file.
-//		            System.out.print("Opening: " + file.getName() + "\n");
-//		            System.out.print("In directory: " + file.getAbsolutePath() + "\n");
-//		            } else {
-//		        	file = fc.getSelectedFile();
-//		        	System.out.print("Can't opening: " + file.getName() + "\n");
-		        }
-			}
-	
-	public class SaveItemAction implements ActionListener {
-		public void actionPerformed(ActionEvent evt) {
-			System.out.print("saveItem\n");
-//			//	exist?
-			//	if true {save(getFileMap)}
-			//	else 
-			//		saveas
-		
+    addSeparator();
 
-			
-//			int returnVal = fc.showSaveDialog(openDialog);
-//				if (returnVal == JFileChooser.APPROVE_OPTION) {
-//					file = fc.getSelectedFile();//This is where a real application would open the file.
-//		            System.out.print("Saving: " + file.getName() + "\n");
-//		            System.out.print("In directory: " + fc.getCurrentDirectory().getName() + "\n");
-//		            
-//		            } else {
-//		        	file = fc.getSelectedFile();
-//		        	System.out.print("Can't save: " + file.getName() + "\n");
-		        }
-			}
-	
-	public class SaveAsItemAction implements ActionListener {
-		public void actionPerformed(ActionEvent evt) {
-			System.out.print("saveAsItem\n");
-			//	int returnVal = fc.showOpenDialog(openDialog);
-			//	if (returnVal == JFileChooser.APPROVE_OPTION) {
-			//	open()
-			//	} else {
-			//	
-			// }
-			}
-		}
-	
-	// appeler existeFileStat => Map
-	// saveStat => StatManager
-	public class ExportStatItemAction implements ActionListener {
-		public void actionPerformed(ActionEvent evt) {
-			System.out.print("exportStatItem\n");
-			}
-		}
-	
-	public class ExitItemAction implements ActionListener {
-		public void actionPerformed(ActionEvent evt) {
-			System.out.print("exitItem\n");
-			}
-		}
+    exitItem.addActionListener(new ExitItemAction());
+    add(exitItem);
+  }
+
+  public class NewItemAction implements ActionListener {
+    public void actionPerformed(ActionEvent evt) {
+      if (debug == true) System.out.print("newItem\n");
+      MapPanel.controller.eventNewFile();
+      // check si chgment  depuis dernier save ou si fichier non save
+      // si oui fenetre;
+      // si user ok
+      // save
+      // sinon quitte
+
+      //	if ((b = comparetosavedFilemap()) {
+      //		openWinDiag()
+      //		if (ok) {
+      //		openSave();
+      //		}
+      //	}
+      // exit
+    }
+  }
+
+  // appeler existeFileMap => Map
+  // saveMap => FileManager
+  // loadMap = FileManager
+  public class OpenItemAction implements ActionListener {
+    public void actionPerformed(ActionEvent evt) {
+      if (debug == true) System.out.print("openItem\n");
+      JFileChooser chooser = new JFileChooser();
+
+      int returnVal = chooser.showOpenDialog(null);
+      if (returnVal == JFileChooser.APPROVE_OPTION) {
+	MapPanel.controller.eventSaveMap(new File(chooser.getSelectedFile().getAbsolutePath()));
+      }
+    }
+  }
+
+  public class SaveItemAction implements ActionListener {
+    public void actionPerformed(ActionEvent evt) {
+      if (debug == true) System.out.print("saveItem\n");
+      JFileChooser chooser = new JFileChooser();
+
+      int returnVal = chooser.showOpenDialog(null);
+      if (returnVal == JFileChooser.APPROVE_OPTION) {
+	MapPanel.controller.eventSaveMap(new File(chooser.getSelectedFile().getAbsolutePath()));
+      }
+    }
+  }
+
+  public class SaveAsItemAction implements ActionListener {
+    public void actionPerformed(ActionEvent evt) {
+      if (debug == true) System.out.print("saveAsItem\n");
+      //	int returnVal = fc.showOpenDialog(openDialog);
+      //	if (returnVal == JFileChooser.APPROVE_OPTION) {
+      //	open()
+      //	} else {
+      //
+      // }
+    }
+  }
+
+  // appeler existeFileStat => Map
+  // saveStat => StatManager
+  public class ExportStatItemAction implements ActionListener {
+    public void actionPerformed(ActionEvent evt) {
+      if (debug == true) System.out.print("exportStatItem\n");
+    }
+  }
+
+  public class ExitItemAction implements ActionListener {
+    public void actionPerformed(ActionEvent evt) {
+      if (debug == true) System.out.print("exitItem\n");
+      System.exit(0);
+    }
+  }
 }
